f4348d689e86c29e188e7e9e808a48ff8b94c0d8
NewGrafon <ultrafig73@gmail.com>
2023-12-18 00:15:56 +0400

    init

--- src/app/pages/orders-list/orders-list.component.ts
+++ src/app/pages/orders-list/orders-list.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import {IAppUser} from "../../app.component";
+
+export const enum OrderType {
+  Current = "Активные заказы",
+  Created = "Созданные вами заказы",
+  Other = "Остальное"
+}
+
+export interface IOrder {
+  id: number;
+  code: string;
+  color: string;
+  colorCode: string;
+  amount: number;
+  deliverDesk: string;
+  type: OrderType;
+  createdBy: IAppUser;
+}
 
 @Component({
   selector: 'app-orders-list',
@@ -8,5 +26,23 @@ import { Component } from '@angular/core';
   styleUrl: './orders-list.component.scss'
 })
 export class OrdersListComponent {
+  readonly orders: IOrder[] = [];
 
+
+  getById(id: number) {
+    return this.orders.find((order) => order.id === id);
+  }
+
+  get byGroup() {
+    return this.orders.reduce((group, order: IOrder) => {
+      // @ts-ignore
+      if (!group[order.type]) {
+        // @ts-ignore
+        group[order.type] = [];
+      }
+      // @ts-ignore
+      group[order.type].push(order);
+      return group;
+    }, {});
+  }
 }
