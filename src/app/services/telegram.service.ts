e46c74a0773ea595deb11d7246b43e18fb225c78
NewGrafon <ultrafig73@gmail.com>
2023-12-18 00:15:42 +0400

    init

--- src/app/services/telegram.service.ts
+++ src/app/services/telegram.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
+
+interface TgButton {
+  show(): void;
+  hide(): void;
+  setText(text: string): void;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TelegramService {
+  private window;
+  tg;
+  constructor(@Inject(DOCUMENT) private _document: Document) {
+    this.window = this._document.defaultView;
+    // @ts-ignore
+    this.tg = this.window.Telegram.WebApp;
+  }
 
-  constructor() { }
+  get MainButton(): TgButton {
+    return this.tg.MainButton;
+  }
 }
