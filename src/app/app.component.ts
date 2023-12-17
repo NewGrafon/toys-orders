1a1d4601988cb9e970148544f23d393b3ec59ff6
NewGrafon <ultrafig73@gmail.com>
2023-12-18 00:15:59 +0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {TelegramService} from "./services/telegram.service";
+import {HeaderComponent} from "./pages/header/header.component";
+
+export const enum UserRole {
+  Worker = "Сборщик",
+  Deliver = "Доставщик",
+  Admin = "Админ",
+}
+
+export type IAppUser = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  role: UserRole;
+  logged: boolean;
+} | undefined;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ToysOrdersAngular';
-  items = [1, 2, 3, 4 , 5];
+export class AppComponent implements OnInit {
+  private _appUser: IAppUser;
+
+  public get appUser(): IAppUser {
+    return this._appUser;
+  }
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    //
+  }
 }
