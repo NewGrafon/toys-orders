89c31e23a563f75e9e0f2c4576a9c54b53050ebb
NewGrafon <ultrafig73@gmail.com>
2023-12-18 00:16:05 +0400

    init

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TelegramService} from "../../services/telegram.service";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
 
+  authForm = new FormGroup({
+    id: new FormControl('', [Validators.required, Validators.min(0)]),
+    password: new FormControl('', Validators.required),
+  })
+
+  get idIsValid(): boolean {
+    return !(this.authForm.controls.id.invalid && this.authForm.controls.id.touched);
+  }
+
+  get passwordIsValid(): boolean {
+    return !(this.authForm.controls.password.invalid && this.authForm.controls.password.touched);
+  }
+
+  onSubmit() {
+
+  }
+
+  constructor(private telegram: TelegramService) {
+    this.telegram?.MainButton?.show();
+  }
 }
