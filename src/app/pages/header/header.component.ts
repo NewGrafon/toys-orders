e680a06d8d553f9efd88daccb13c331af79d8e93
NewGrafon <ultrafig73@gmail.com>
2023-12-20 02:18:01 +0400

    current url sub

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
-import {AppComponent, IAppUser} from "../../app.component";
+import { AppComponent, IAppUser } from '../../app.component';
+import { Router } from '@angular/router';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    JsonPipe,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private _currentUrl: string = '/';
+  get currentUrl() {
+    return this._currentUrl;
+  }
+
   get user(): IAppUser {
-    console.log(1)
     return this.appComponent.appUser;
   }
-  constructor(private readonly appComponent: AppComponent) {}
+
+  constructor(
+    private readonly appComponent: AppComponent,
+    private readonly router: Router,
+  ) {
+    AppComponent.WaitForUpdateUser((url) => {
+      this._currentUrl = url;
+    });
+  }
 }
