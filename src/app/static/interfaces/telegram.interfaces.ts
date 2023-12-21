8e2d6a70556ea5e3d6704fddcced092b41de9948
NewGrafon <ultrafig73@gmail.com>
2023-12-21 15:43:53 +0400

    ,

---
+++ src/app/static/interfaces/telegram.interfaces.ts
@@ -0,0 +1,25 @@
+export interface TgButton {
+  show: () => void;
+  hide: () => void;
+  setText: (text: string) => void;
+  onClick: (callback: Function) => void;
+  offClick: (callback: Function) => void;
+  showProgress: (leaveActive: boolean) => void;
+  hideProgress: () => void;
+}
+
+// export interface TgPopup {
+//   showPopup: (tgPopupParams: TgPopupParams, callback: Function) => void;
+// }
+
+export interface TgPopupParams {
+  title?: string;
+  message: string;
+  buttons?: TgPopupButton[];
+}
+
+export interface TgPopupButton {
+  id?: string;
+  type?: 'default' | 'ok' | 'close' | 'cancel' | 'destructive';
+  text: string;
+}
