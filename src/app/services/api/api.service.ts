827d0abd003422856674fc574eedb6fa13720298
NewGrafon <ultrafig73@gmail.com>
2023-12-21 15:42:28 +0400

    fix auth, add userInfo

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -2,26 +2,40 @@ import { Injectable } from '@angular/core';
 import ky from 'ky';
 import { type IApiAuth, type IApiAuthResponse } from '../../static/interfaces/auth.interface';
 import { CookieService } from 'ngx-cookie-service';
-import { AuthComponent } from '../../pages/auth/auth.component';
+import { COOKIE_TOKEN } from '../../static/consts/token.const';
+import { TelegramService } from '../telegram/telegram.service';
+import { IAppUser } from '../../static/types/app-user.type';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(private readonly cookieService: CookieService) {
+  constructor(
+    private readonly cookieService: CookieService,
+    private readonly tg: TelegramService,
+  ) {
   }
 
   private readonly api = ky.create({
-    prefixUrl: 'api.toys-orders.ru/',
+    prefixUrl: 'https://toys-orders.ru/',
     headers: {
-      authorization: this.cookieService.get('access_token')
+      authorization: this.cookieService.get(COOKIE_TOKEN)
     },
     retry: 0,
     hooks: {
       afterResponse: [
-        async (res) => {
+        async (_input, _options, res) => {
           const body = await res.json();
-          console.log(body);
+          if (body.statusCode >= 400 && body.statusCode < 500 && !_input.url.includes('users/me')) {
+            this.tg.showPopup({
+              title: 'Ошибка',
+              message: body.message as string,
+              buttons: [{
+                type: 'ok',
+                text: 'Ок'
+              }]
+            })
+          }
         }
       ]
     }
@@ -31,14 +45,35 @@ export class ApiService {
     let result: IApiAuthResponse;
     try {
       result = await this.api
-        .post('auth', {
+        .post('auth/login', {
           json: body,
         }).json();
+      if (result?.session_token === undefined) {
+        result.session_token = null;
+      }
+      return result;
     } catch (e) {
       console.error(e);
       result = {
         session_token: null,
       };
+    }
+
+    return result;
+  }
+
+  public async userInfo(): Promise<IAppUser> {
+    let result: IAppUser;
+
+    try {
+      result = await this.api
+        .get('users/me').json();
+      if (!result?.id) {
+        result = undefined;
+      }
+    } catch (e) {
+      console.error(e);
+      result = undefined;
     }
 
     return result;
