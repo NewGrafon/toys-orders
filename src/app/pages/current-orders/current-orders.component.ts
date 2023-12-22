86565b414c5bd7d91c0db4203bbb5706547eab68
NewGrafon <ultrafig73@gmail.com>
2023-12-22 20:32:06 +0300

    styles

--- src/app/pages/current-orders/current-orders.component.ts
+++ src/app/pages/current-orders/current-orders.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-current-orders',
   standalone: true,
   imports: [],
-  templateUrl: './current-order.component.html',
+  templateUrl: './current-orders.component.html',
   styleUrl: './current-orders.component.scss',
 })
-export class CurrentOrderComponent {
+export class CurrentOrdersComponent {
 
 }
