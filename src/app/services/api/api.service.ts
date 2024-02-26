85e060437e5dd8ace443bd123bdae21b02719548
NewGrafon <ultrafig73@gmail.com>
2024-02-26 19:28:25 +0300

    fixes for backend server changes

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
 import ky from 'ky';
-import { type IApiAuth, type IApiAuthResponse } from '../../static/interfaces/auth.interface';
+import {
+  type IApiAuth,
+  type IApiAuthResponse,
+} from '../../static/interfaces/auth.interface';
 import { CookieService } from 'ngx-cookie-service';
 import { COOKIE_TOKEN } from '../../static/consts/token.const';
 import { TelegramService } from '../telegram/telegram.service';
 import { IAppUser } from '../../static/types/app-user.type';
-import { IApiCreateUser, IApiEditUser } from '../../static/interfaces/create-user.interfaces';
-import { IApiCreateOrder, IOrder } from '../../static/interfaces/order.interface';
+import {
+  IApiCreateUser,
+  IApiEditUser,
+} from '../../static/interfaces/create-user.interfaces';
+import {
+  IApiOrdersByTimestamp,
+  IOrder,
+} from '../../static/interfaces/order.interfaces';
 import { Router } from '@angular/router';
+import {
+  IApiToyResponse,
+  ICart,
+  ICartToy,
+} from '../../static/interfaces/cart.interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -32,10 +46,12 @@ export class ApiService {
               this.tg.showPopup({
                 title: 'Ошибка',
                 message: body.message as string,
-                buttons: [{
-                  type: 'ok',
-                  text: 'Ок',
-                }],
+                buttons: [
+                  {
+                    type: 'ok',
+                    text: 'Ок',
+                  },
+                ],
               });
               await this.router.navigateByUrl('/');
             }
@@ -49,8 +65,7 @@ export class ApiService {
     private readonly cookieService: CookieService,
     private readonly tg: TelegramService,
     private readonly router: Router,
-  ) {
-  }
+  ) {}
 
   public async auth(body: IApiAuth): Promise<IApiAuthResponse> {
     let result: IApiAuthResponse;
@@ -58,9 +73,10 @@ export class ApiService {
       result = await this.api
         .post('auth/login', {
           json: body,
-        }).json();
-        console.log(result);
-        
+        })
+        .json();
+      console.log(result);
+
       if (result?.session_token === undefined) {
         result.session_token = null;
       }
@@ -80,8 +96,7 @@ export class ApiService {
     let result: IAppUser;
 
     try {
-      result = await this.api
-        .get('users/me').json();
+      result = await this.api.get('users/me').json();
       if (!result?.id) {
         result = undefined;
       }
@@ -97,7 +112,7 @@ export class ApiService {
     let result: IAppUser[];
 
     try {
-      result = await this.api.get('users/get_all').json() || [];
+      result = (await this.api.get('users/get_all').json()) || [];
     } catch (e) {
       console.error(e);
       result = [];
@@ -110,9 +125,11 @@ export class ApiService {
     let result: boolean;
 
     try {
-      result = await this.api.post('users/create', {
-        json: body,
-      }).json();
+      result = await this.api
+        .post('users/create', {
+          json: body,
+        })
+        .json();
     } catch (e) {
       console.error(e);
       result = false;
@@ -127,9 +144,11 @@ export class ApiService {
     try {
       console.log(`users/edit/${userId}`);
 
-      result = await this.api.patch(`users/edit/${userId}`, {
-        json: body,
-      }).json();
+      result = await this.api
+        .patch(`users/edit/${userId}`, {
+          json: body,
+        })
+        .json();
     } catch (e) {
       console.error(e);
       result = false;
@@ -151,8 +170,75 @@ export class ApiService {
     return result;
   }
 
-  public async getAllOrders(): Promise<IOrder[]> {
-    let result: IOrder[];
+  // need test
+  public async changeAmountInCart(cartToy: ICartToy): Promise<IAppUser> {
+    let result: IAppUser;
+
+    try {
+      result = await this.api
+        .post(`orders/change_in_cart`, {
+          json: cartToy,
+        })
+        .json();
+    } catch (e) {
+      console.error(e);
+      result = undefined;
+    }
+
+    return result;
+  }
+
+  // need test
+  public async removeFromCart(cartToy: ICartToy): Promise<IAppUser> {
+    let result: IAppUser;
+
+    try {
+      result = await this.api
+        .delete(`orders/remove_from_cart`, {
+          json: cartToy,
+        })
+        .json();
+    } catch (e) {
+      console.error(e);
+      result = undefined;
+    }
+
+    return result;
+  }
+
+  // need test
+  public async getAllToys(): Promise<IApiToyResponse[]> {
+    let result: IApiToyResponse[];
+
+    try {
+      result = await this.api.get('toys/get_all').json();
+    } catch (e) {
+      console.error(e);
+      result = [];
+    }
+
+    return result;
+  }
+
+  // need test
+  public async getToyById(
+    id: number | string,
+  ): Promise<IApiToyResponse | undefined> {
+    let result: IApiToyResponse | undefined;
+
+    try {
+      result = await this.api.get(`toys/${id}`).json();
+    } catch (e) {
+      console.error(e);
+      result = undefined;
+    }
+
+    return result;
+  }
+
+  // need test
+  public async getAllOrders(): Promise<IApiOrdersByTimestamp[]> {
+    let result: IApiOrdersByTimestamp[];
 
     try {
       result = await this.api.get('orders/get_all').json();
@@ -164,13 +250,48 @@ export class ApiService {
     return result;
   }
 
-  public async createOrder(body: IApiCreateOrder): Promise<boolean> {
+  // // TODO
+  // public async createOrder(body: IApiCreateOrder): Promise<boolean> {
+  //   let result: boolean;
+
+  //   try {
+  //     result = await this.api
+  //       .post('orders', {
+  //         json: body,
+  //       })
+  //       .json();
+  //   } catch (e) {
+  //     console.error(e);
+  //     result = false;
+  //   }
+
+  //   return result;
+  // }
+
+  // need test
+  public async confirmCart(cart: ICart): Promise<IOrder[]> {
+    let result: IOrder[];
+
+    try {
+      result = await this.api
+        .post('orders/confirm_cart', {
+          json: cart,
+        })
+        .json();
+    } catch (e) {
+      console.error(e);
+      result = [];
+    }
+
+    return result;
+  }
+
+  // need test
+  public async takeOrders(cartTimestamp: number): Promise<boolean> {
     let result: boolean;
 
     try {
-      result = await this.api.post('orders', {
-        json: body,
-      }).json();
+      result = await this.api.patch(`orders/take/${cartTimestamp}`).json();
     } catch (e) {
       console.error(e);
       result = false;
@@ -179,11 +300,17 @@ export class ApiService {
     return result;
   }
 
-  public async takeOrder(id: number): Promise<boolean> {
+  // need test
+  public async closeOrders(
+    cartTimestamp: number,
+    isFinishedNotCancel: boolean,
+  ): Promise<boolean> {
     let result: boolean;
 
     try {
-      result = await this.api.patch(`orders/take/${id}`).json();
+      result = await this.api
+        .patch(`orders/close/${cartTimestamp}/${isFinishedNotCancel}`)
+        .json();
     } catch (e) {
       console.error(e);
       result = false;
@@ -192,24 +319,12 @@ export class ApiService {
     return result;
   }
 
-  public async closeOrder(id: number, isFinishedNotCancel: boolean): Promise<boolean> {
+  // need test
+  public async cancelOrders(cartTimestamp: number): Promise<boolean> {
     let result: boolean;
 
     try {
-      result = await this.api.patch(`orders/close/${id}/${isFinishedNotCancel}`).json();
-    } catch (e) {
-      console.error(e);
-      result = false;
-    }
-
-    return result;
-  }
-
-  public async cancelOrder(id: number): Promise<boolean> {
-    let result: boolean;
-
-    try {
-      result = await this.api.delete(`orders/cancel/${id}`).json();
+      result = await this.api.delete(`orders/cancel/${cartTimestamp}`).json();
     } catch (e) {
       console.error(e);
       result = false;
